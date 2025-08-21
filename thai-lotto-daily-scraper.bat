ce25a4d2c30918fd7bb4f199bf3fe3a2999d1e61
Noppasit Trisittiwat <noppasittr@webmail.npru.ac.th>
2025-08-21 15:46:41 +0700

    remove ignored files

--- thai-lotto-daily-scraper.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d D:\thai-lotto-scraper
-echo ===== %DATE% %TIME% ===== >> "D:\thai-lotto-scraper\debug_log.txt"
-call "C:\Users\noppa\anaconda3\Scripts\activate.bat" Playground
-python "D:\thai-lotto-scraper\thai-lotto-daily-scraper-viewsource.py" >> "D:\thai-lotto-scraper\debug_log.txt" 2>&1
-echo. >> "D:\thai-lotto-scraper\debug_log.txt"
